192733a26dd34737816a6a5370f4553f0a59435f
goldcollar95 <yjs7298@naver.com>
2022-11-30 16:33:49 +0900

    feat(BaseBall): 기능구현 createRandomNumbers (V)

--- src/main/java/baseball/domain/NumberGenerator.java
+++ src/main/java/baseball/domain/NumberGenerator.java
@@ -5,24 +5,23 @@ import java.util.List;
 import java.util.Random;
 
 public class NumberGenerator {
-    public List<Integer> createRandomNumbers(){
+    public List<Integer> createRandomNumbers() {
         // 3개의 숫자가 담길 때까지
         // 만약 이미 존재하는 숫자라면 담지 않는다.
         // 만약 존재하지 않는 숫자라면 담는다.
         List<Integer> numbers = new ArrayList<>();
-        while(numbers.size() < 3){
+        while (numbers.size() < 3) {
 
-            for(int i = 0; i < 3; i++){
+            for (int i = 0; i < 3; i++) {
                 int number = new Random().nextInt(9) + 1;
-                if(numbers.contains(number)){
+                if (numbers.contains(number)) {
                     continue;
-                }else{
+                } else {
                     numbers.add(number);
                 }
-                numbers.add(number);
+
             }
-
-
+        }
         return numbers;
     }
 }
