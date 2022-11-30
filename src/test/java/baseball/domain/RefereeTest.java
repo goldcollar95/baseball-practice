e332dc10394ea7542dc9eaec8759957489816e8b
goldcollar95 <yjs7298@naver.com>
2022-12-01 01:11:30 +0900

    test(): Arrays.asList(1,2,3) 부분 -> 클래스 변수에 불변의 값 ANSWER로 모두 통일

--- src/test/java/baseball/domain/RefereeTest.java
+++ src/test/java/baseball/domain/RefereeTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -12,6 +13,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class RefereeTest {
     private Referee referee;
+    private static final List<Integer> ANSWER = Arrays.asList(1,2,3);
 
     @BeforeEach
     void setUp() {
@@ -20,29 +22,25 @@ class RefereeTest {
 
     @Test
     void 스트라이크3() {
-
-        String result = referee.compare(Arrays.asList(1,2,3), Arrays.asList(1,2,3));
+        String result = referee.compare(ANSWER, Arrays.asList(1,2,3));
         assertThat(result).isEqualTo("0 볼 3 스트라이크");
     }
 
     @Test
     void 낫싱() {
-
-        String result = referee.compare(Arrays.asList(1, 2, 3), Arrays.asList(7, 8, 9));
+        String result = referee.compare(ANSWER, Arrays.asList(7, 8, 9));
         assertThat(result).isEqualTo("낫싱");
     }
 
     @Test
     void 볼3() {
-
-        String result = referee.compare(Arrays.asList(1, 2, 3), Arrays.asList(2, 3, 1));
+        String result = referee.compare(ANSWER, Arrays.asList(2, 3, 1));
         assertThat(result).isEqualTo("3 볼 0 스트라이크");
     }
 
     @Test
     void 볼2_스트라이크1() {
-
-        String result = referee.compare(Arrays.asList(1, 2, 3), Arrays.asList(1, 3, 2));
+        String result = referee.compare(ANSWER, Arrays.asList(1, 3, 2));
         assertThat(result).isEqualTo("2 볼 1 스트라이크");
     }
 
