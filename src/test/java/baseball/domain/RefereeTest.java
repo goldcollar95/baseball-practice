30a6e4667c15baa8c7444eb2d5063cf6ad7b7aa4
goldcollar95 <yjs7298@naver.com>
2022-12-01 01:09:09 +0900

    test refactor(): Referee객체 중복 제거 -> BeforeEach setUp함수 사용

--- src/test/java/baseball/domain/RefereeTest.java
+++ src/test/java/baseball/domain/RefereeTest.java
@@ -1,5 +1,6 @@
 package baseball.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -7,31 +8,40 @@ import java.util.Arrays;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+
+
 class RefereeTest {
+    private Referee referee;
+
+    @BeforeEach
+    void setUp() {
+        referee = new Referee();
+    }
+
     @Test
     void 스트라이크3() {
-        Referee referee = new Referee();
+
         String result = referee.compare(Arrays.asList(1,2,3), Arrays.asList(1,2,3));
         assertThat(result).isEqualTo("0 볼 3 스트라이크");
     }
 
     @Test
     void 낫싱() {
-        Referee referee = new Referee();
+
         String result = referee.compare(Arrays.asList(1, 2, 3), Arrays.asList(7, 8, 9));
         assertThat(result).isEqualTo("낫싱");
     }
 
     @Test
     void 볼3() {
-        Referee referee = new Referee();
+
         String result = referee.compare(Arrays.asList(1, 2, 3), Arrays.asList(2, 3, 1));
         assertThat(result).isEqualTo("3 볼 0 스트라이크");
     }
 
     @Test
     void 볼2_스트라이크1() {
-        Referee referee = new Referee();
+
         String result = referee.compare(Arrays.asList(1, 2, 3), Arrays.asList(1, 3, 2));
         assertThat(result).isEqualTo("2 볼 1 스트라이크");
     }
