0927abd75b9e1574f46602daf9f4283415623237
goldcollar95 <yjs7298@naver.com>
2022-12-01 01:02:53 +0900

    test(RefereeTest): 낫싱, 볼3, 볼2_스트라이크1 각각 들어올때 통과시킬수 있는 테스트 코드 구현

--- src/test/java/baseball/domain/RefereeTest.java
+++ src/test/java/baseball/domain/RefereeTest.java
@@ -14,4 +14,26 @@ class RefereeTest {
         String result = referee.compare(Arrays.asList(1,2,3), Arrays.asList(1,2,3));
         assertThat(result).isEqualTo("0 볼 3 스트라이크");
     }
+
+    @Test
+    void 낫싱() {
+        Referee referee = new Referee();
+        String result = referee.compare(Arrays.asList(1, 2, 3), Arrays.asList(7, 8, 9));
+        assertThat(result).isEqualTo("낫싱");
+    }
+
+    @Test
+    void 볼3() {
+        Referee referee = new Referee();
+        String result = referee.compare(Arrays.asList(1, 2, 3), Arrays.asList(2, 3, 1));
+        assertThat(result).isEqualTo("3 볼 0 스트라이크");
+    }
+
+    @Test
+    void 볼2_스트라이크1() {
+        Referee referee = new Referee();
+        String result = referee.compare(Arrays.asList(1, 2, 3), Arrays.asList(1, 3, 2));
+        assertThat(result).isEqualTo("2 볼 1 스트라이크");
+    }
+
 }
