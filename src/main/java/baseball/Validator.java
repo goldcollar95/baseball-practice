6c0deb2415c4dfbc104af2549b02938841a6d49b
goldcollar95 <yjs7298@naver.com>
2022-12-02 11:10:01 +0900

    feat(baseball): ValidatorList implement

--- src/main/java/baseball/Validator.java
+++ src/main/java/baseball/Validator.java
@@ -1,4 +1,29 @@
 package baseball;
 
 public class Validator {
+
+    public void ValidatorRangeNumber(String number){
+        for(int i = 0; i < number.length(); i++){
+            if(number.length() != 3 ) {
+            throw new IllegalArgumentException("3자리의 숫자만 입력 가능합니다.");
+            }
+        }
+    }
+
+    public void ValidatorNotEmpty(String number){
+        for(int i = 0; i < number.length(); i++){
+            if(number.charAt(i) == ' '){
+                throw new IllegalArgumentException("공백은 들어올 수 없습니다");
+            }
+        }
+    }
+
+    public void ValidatorOnlyNumber(String number){
+        for(int i = 0; i < number.length(); i++){
+            if(number.length() == 0){
+                throw new IllegalArgumentException("0은 들어올 수 없습니다.")
+            }
+        }
+    }
+
 }
